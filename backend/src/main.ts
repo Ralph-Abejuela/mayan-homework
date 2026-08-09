f7bad9c1c1124666d29d664f4f2846d2b54e3d54
ralph-abejuela <abejuela.ralph.balatucan@gmail.com>
2026-08-09 11:42:43 +0800

    Added logging and cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Nest');
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Started on http://localhost:${process.env.PORT}`);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
