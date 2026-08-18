a65356346c1bdbcaa7b61d86a229037c0b02bd47
ralph-abejuela <abejuela.ralph.balatucan@gmail.com>
2026-08-18 17:51:47 +0800

    Added trim title transform

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -1,8 +1,17 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTaskDto {
   @IsString({ message: 'title must be a string.' })
   @IsNotEmpty({ message: 'title must not be empty.' })
+  @Transform(
+    ({ value }) => (
+      typeof value === 'string' ? value.trim() : value,
+      {
+        toClassOnly: true,
+      }
+    ),
+  )
   readonly title!: string;
   @IsString()
   readonly description?: string;
