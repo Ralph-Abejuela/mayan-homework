2b672bf2c059ae4968182e96a9356bd9a1d205ee
ralph-abejuela <abejuela.ralph.balatucan@gmail.com>
2026-08-18 17:56:05 +0800

    removed toClassOnly

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -4,14 +4,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateTaskDto {
   @IsString({ message: 'title must be a string.' })
   @IsNotEmpty({ message: 'title must not be empty.' })
-  @Transform(
-    ({ value }) => (
-      typeof value === 'string' ? value.trim() : value,
-      {
-        toClassOnly: true,
-      }
-    ),
-  )
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   readonly title!: string;
   @IsString()
   readonly description?: string;
